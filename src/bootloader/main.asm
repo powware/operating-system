2b0240bb0b0b625243b07435486bd364e8fd9b17
powware <m4chau3r@gmail.com>
2021-11-17 01:50:19 +0100

    refactor

--- src/bootloader/main.asm
+++ src/bootloader/main.asm
@@ -18,8 +18,8 @@ Start: ;DH and ES:DI should be preserved by the MBR for full Plug-and-Play suppo
 
     cld                     ;clear direction flag for string operations
 
-PrintDiskNumber:
-    mov si, disk_number
+PrintBootDiskNumber:
+    mov si, boot_disk_number
     call PrintString
     mov bl, dl
     call PrintByteAsHex
@@ -29,46 +29,43 @@ ResetDiskSystem:
     xor al, al
     int 0x13
     jnc ResetDiskSystem
-    call CheckInterruptReturnCode
+    cmp ah, 0
+    jne ResetDiskSystemError
 
 CheckExtensionPresent:
     mov bx, 0x55AA
     mov ah, 0x41
     int 0x13
 
-    jc BootError
+    jc EDDError
     cmp bx, [magic_number]
-    jne BootError
-    and cx, 0x10
-    jz BootError
-    jmp HALT
-
-BootError:
-    call PrintNewline
-    mov bl, ch
-    call PrintByteAsHex
-    mov bl, cl
-    call PrintByteAsHex
-    call PrintNewline
-    mov si, boot_error
-    call PrintString
+    jne EDDError
+    and cx, 0x04
+    jz EDDError
 
 HALT:
     cli
     hlt
 
-; ah: return code
-; clears: ax, bh, si
-CheckInterruptReturnCode:
-    cmp ah, 0
-    je .return
-    mov si, interrupt_error
+ResetDiskSystemError:
+    mov si, reset_disk_system_error
     call PrintString
     mov bl, ah
     call PrintByteAsHex
-.return:
-    ret
+    call PrintNewline
+    jmp BootError
 
+EDDError:
+    mov si, edd_error
+    call PrintString
+    call PrintNewline
+    jmp BootError
+
+BootError:
+    mov si, boot_error
+    call PrintString
+    call PrintNewline
+    jmp HALT
 
 ; si: null-terminated string
 ; clears: ax, bh
@@ -76,12 +73,12 @@ PrintString:
     xor bh, bh
     mov ah, 0x0E
 
-.loop:
+.print_char:
     lodsb
     or al, al
     jz .return
     int 0x10
-    jmp .loop
+    jmp .print_char
 
 .return:
     ret
@@ -93,41 +90,35 @@ PrintByteAsHex:
     mov ah, 0x0E
 
     mov al, bl
-    and al, 0xF0
     shr al, 4
+    mov cx, 2
+.print_digit:
     cmp al, 0xA
-    jge .letter1
+    jge .letter
     add al, '0'
-    jmp .print1
-.letter1:
+    jmp .print
+.letter:
     add al, 'A'-0xA
-.print1:
+.print:
     int 0x10
-
     mov al, bl
-    and al, 0x0F
-    cmp al, 0xA
-    jge .letter2
-    add al, '0'
-    jmp .print2
-.letter2:
-    add al, 'A'-0xA
-.print2:
-    int 0x10
+    and al, 0xF
+    loop .print_digit
     ret
 
 ; clears: ax, bh
 PrintNewline:
     xor bh, bh
-    mov ax, 0x0E0A
+    mov ax, 0x0E0A      ; mov al, 0x0A
     int 0x10
     mov al, 0x0D
     int 0x10
     ret
 
 
-disk_number db "Disk number: 0x", 0
-interrupt_error db "Interrupt error: 0x", 0
+boot_disk_number db "Boot Disk Number: 0x", 0
+reset_disk_system_error db "Reset Disk System Error: 0x", 0
+edd_error db "Enhanced Disk Drive not supported", 0
 boot_error db "Boot Error", 0
 ;guid db "pow's bootloader", 0
 
